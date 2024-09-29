9a51425ac117b2f93f5f1e52d8fab36ec08aef43
niall <niall@DESKTOP-CQMMV32>
2024-09-29 15:57:20 -0500

    Changing my fork

---
+++ Java8TutorialClone/src/main/java/com/mycompany/java8tutorialclone/Java8TutorialClone.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java8tutorialclone;
+
+/**
+ *
+ * @author niall
+ */
+public class Java8TutorialClone {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! This is my small alteration");
+    }
+}
